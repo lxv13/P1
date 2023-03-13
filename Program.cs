e6d317789b2520daa0dad6b3637b32ea4a9eb134
lxv13 <mopvaleron@gmail.com>
2023-03-13 09:53:17 +0300

    v1.0

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace УП_ПП1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Address tom = new Address();
+            string addressCity = tom.city;
+            string addressStreet = tom.street;
+            int addressNumber = tom.number;
+            tom.city = "Полоцк";
+            tom.street = "Октябрьская";
+            tom.number = 57;
+            tom.Print();
+        }
+    }
+    class Address
+    {
+        public string city="Undefined";
+        public string street="Undefined";
+        public int number=0;
+
+        public void Print()
+        {
+            Console.WriteLine($"Город: {city}  Улица: {street}   Номер дома: {number}");
+        }
+    }
+}
